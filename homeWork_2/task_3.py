d64ec583317611301a132411c554baec7392b4db
GumarGarifullyn <118556134+GumarGarifullyn@users.noreply.github.com>
2023-02-26 00:26:28 +0300

    task3

--- homeWork_2/task_3.py
+++ homeWork_2/task_3.py
@@ -1,20 +1,7 @@
-import math
-sum = int(input("Введите сумму двух числе x и y: "))
-mult = int(input("Введите произведение двух числе x и y: "))
-disc = sum**2 - 4*mult
-if disc < 0:
-    print("Невозможно подобрать x и y для заданных вами параметров")
-elif disc > 0:
-    print("Возможны 2 варианта решения:", end = " ")
-    x = (sum + math.sqrt(disc))/2*mult
-    y = sum - x
-    print(x, y, end = " ")
-    x = (sum - math.sqrt(disc))/2*mult
-    y = sum - x
-    print(x, y)
-else:
-    print("Возможен лишь 1 вариант решения:", end = " ")
-    x = (sum - math.sqrt(disc))/2*mult
-    y = sum - x
-    print(x, y)
-    
+# Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+n = int(input("Введите число: "))
+m = 0
+while 2**m <= n:
+    print(2**m, end=" ")
+    m += 1
